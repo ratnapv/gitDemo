bce352d00c0758b324381d6f0b1c99b1f83d8447
Ratnakumar <Ratnakumar@WSHYD-021.WILCO.COM>
2018-11-28 17:12:26 +0530

    New project Added

---
+++ GitDemoProject/src/git/demo/TestClass.java
@@ -0,0 +1,11 @@
+package git.demo;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("First line from Class1");
+	}
+
+}
