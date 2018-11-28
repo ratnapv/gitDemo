3179031b8ce84adc4d56ea5fa8639cb5ebd5f10c
Ratnakumar <Ratnakumar@WSHYD-021.WILCO.COM>
2018-11-28 17:26:05 +0530

    test

--- venkateshgit/src/com/sample/test1.java
+++ venkateshgit/src/com/sample/test1.java
@@ -4,7 +4,8 @@ public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("venkat");
+		System.out.println("venkat");
+		System.out.println("Testing");
 	}
 
 }
